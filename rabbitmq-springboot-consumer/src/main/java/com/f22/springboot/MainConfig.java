2fe6be39631a18592c73f063403a5768ce43d946
lmw <pinkpig_zsy@163.com>
2021-09-26 00:16:59 +0800

    init commit

---
+++ rabbitmq-springboot-consumer/src/main/java/com/f22/springboot/MainConfig.java
@@ -0,0 +1,10 @@
+package com.f22.springboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.f22.springboot.*"})
+public class MainConfig {
+
+}
